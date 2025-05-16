6dd5124253401658a1a707a2d65f68297369c7c6
FujiharaAsuka <241070@std.hi-joho.ac.jp>
2025-05-16 11:33:59 +0900

    java課題１

---
+++ src/Reivew1.java
@@ -0,0 +1,9 @@
+public class Reivew1 {
+    public static void main (String[] args){
+        System.out.print("こんにちは。\n");
+        System.out.println("今日の天気は\t晴れです。");
+        System.out.println("今日は一日\n洗濯日和です。");
+        System.out.println("外に干しても大丈夫です。");
+        System.out.println("文字は\'文字列は\"で囲みましょう");
+    }
+}
