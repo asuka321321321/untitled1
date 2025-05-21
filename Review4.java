39e22187fcbe2eede449228706b94c25e64e5328
FujiharaAsuka <241070@std.hi-joho.ac.jp>
2025-05-21 09:41:12 +0900

    java4

---
+++ Review4.java
@@ -0,0 +1,13 @@
+public class Review4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String str = "88" + "88";
+        final double PI = 3.141592;
+        float cric = 3.14;
+        long a = 10000000000;
+        boolean flg = "true";
+        byte b = 35;
+        int num = 0735;
+        char c = 'a';
+    }
+}
