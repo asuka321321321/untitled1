19b0450d40cc03352012bcd8ed7d939776a8e706
Fujihara <241070@std.hi-joho.ac.jp>
2025-06-02 01:00:48 +0900

    Review20

--- src/Reviews/Review2/Review20.java
+++ src/Reviews/Review2/Review20.java
@@ -2,7 +2,7 @@ package Reviews.Review2;
 
 public class Review20 {
     public static void main(String[] args) {
-        int num = 100;
+        int num = 10;
         int sum = 0;
         for (int i = 0; i <= num; i++) {
             sum += i;
