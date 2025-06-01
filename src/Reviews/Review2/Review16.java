12b2074d2e107f0732c4c06068d8f36651951063
Fujihara <241070@std.hi-joho.ac.jp>
2025-06-02 00:57:59 +0900

    Review16

---
+++ src/Reviews/Review2/Review16.java
@@ -0,0 +1,14 @@
+package Reviews.Review2;
+
+public class Review16 {
+    public static void main(String[] args) {
+        int num = 100;
+        int sum = 0;
+        int i = 1;
+        while (i <= num) {
+            sum += i;
+            i++;
+        }
+        System.out.println("結果：" + sum);
+    }
+}
