07d61aa460fe63c41daba1a5eb3e954a62b8f659
Fujihara <241070@std.hi-joho.ac.jp>
2025-06-02 00:58:29 +0900

    Review19

---
+++ src/Reviews/Review2/Review19.java
@@ -0,0 +1,14 @@
+package Reviews.Review2;
+
+public class Review19 {
+    public static void main(String[] args) {
+        int num = 100;
+        int i = 0;
+        int factorial = 1;
+        while (i < num) {
+            factorial *= 1;
+            i++;
+        }
+        System.out.println("結果:" + factorial);
+    }
+}
